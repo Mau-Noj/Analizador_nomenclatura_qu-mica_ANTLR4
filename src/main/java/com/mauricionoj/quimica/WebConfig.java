9cadb1d1cb9d972fefc27ee410a3b09df7be567a
Mauricio_Noj <superpepitopro64@gmail.com>
2026-03-13 22:48:25 -0600

    [FIX] Arreglando CORS

---
+++ src/main/java/com/mauricionoj/quimica/WebConfig.java
@@ -0,0 +1,22 @@
+package com.mauricionoj.quimicaanalizador;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addMapping("/**")
+                    .allowedOrigins("*") // Esto permite que cualquier origen conecte
+                    .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                    .allowedHeaders("*")
+                    .allowCredentials(false);
+        }
+    }
+}
